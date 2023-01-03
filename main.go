7410548a22fec7e19b7df8f02cfe21952e3cbd8e
fojtas98 <fojtik.jan98@gmail.com>
2023-01-03 17:05:42 +0100

    fixing issues

--- main.go
+++ main.go
@@ -3,19 +3,28 @@ package main
 import (
 	"context"
 	"fmt"
+	"strconv"
+	"strings"
 	"time"
 
 	"example.com/db"
-	"example.com/scraper"
+	"github.com/gocolly/colly"
 )
 
 func main() {
 	ctx := context.Background()
 
 	bunDb := db.ConnectToDB(ctx)
-	numberOfPeople, c := scraper.CreateScraper()
 	i := 1
+	var c = colly.NewCollector()
 	for {
+		numberOfPeople := 0
+		c.OnXML("/html/body/header/div[1]/div[2]/div/div[3]/div/div/text()", func(x *colly.XMLElement) {
+			v := strings.TrimSpace(x.Text)
+			if v != "" {
+				numberOfPeople, _ = strconv.Atoi(v)
+			}
+		})
 		c.Visit("https://www.nafialce.cz/bazen-sauny")
 		fmt.Printf("looping for %d time\n", i)
 		ava := &db.Availibility{NumberOfPeople: numberOfPeople, Time: time.Now()}
